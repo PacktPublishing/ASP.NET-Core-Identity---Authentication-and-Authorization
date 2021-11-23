aa9a7ee18fcd9cd084c1a8a3c5c285ff0c486214
ClaraJosephPackt <clarajoseph@packt.com>
2021-11-23 10:28:32 +0530

    Code Files are placed

---
+++ Classifieds.Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Classifieds.Data.Configuration;
+using Classifieds.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classifieds.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Advertisement> Advertisements { get; set; }
+    }
+}
